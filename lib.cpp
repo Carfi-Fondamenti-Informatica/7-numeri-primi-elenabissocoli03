d59f6edcea98d5680ba42e2de9d77421541f1ccd
elenabissocoli03 <91657260+elenabissocoli03@users.noreply.github.com>
2021-12-01 23:29:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+bool ric (int i, int n){
+  if (i==n) {
+      return true;
+  }else if (n==1){
+      return true;
+  }else if(n%1==0){
+      return false;
+  }else{
+      return ric(i+1,n);
+  }
 
+}
+//
+// Created by Utente on 01/12/2021.
+//
