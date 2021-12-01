60e4391dfd58c45e2fcc253be143e90dfe7a7ed1
elenabissocoli03 <91657260+elenabissocoli03@users.noreply.github.com>
2021-12-01 23:28:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int n=0;
+    cin>> n;
+    if (ric(2,n)){
+        cout<< "numero primo"<< endl;
+    }else{
+        std::cout<< "numero non primo"<<endl;
+    }
+    return 0;
 }
